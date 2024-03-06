2e50e09a332c043ed78e91206240448e9dc5bc0b
Ahmad Amman <132729640+Ahmad-s2004@users.noreply.github.com>
2024-03-06 22:42:04 +0500

    Update Accessories.js

--- src/pages/Accessories/Accessories.js
+++ src/pages/Accessories/Accessories.js
@@ -28,9 +28,9 @@ const Accessories = () => {
               return (
                 <div className="col-lg-3 col-md-4 col-sm-6 my-2w" key={ind}>
                   <div className="card">
-                    <img src={`https://sapphire-back.vercel.app/api/v3/auth/${x.img}`} className="card-img-top" alt="sas" />
+                    <img src={`https://sapphire-back.vercel.app/${x.img}`} className="card-img-top" alt="sas" />
                     <div className="card-body">
-                    <h5 className="card-title fnt-mont fnt-title">{x.title}</h5>
+                    <h5 className="card-title fnt-mont fnt-title">{x.img}{x.title}</h5>
                       <div className="d-flex">
                       <p className="card-text h6 col-6">Rs.{x.price}</p>
                       <Link to={`/accessories/${x._id}`} className="btn btn-dark btn-margin col-6">View Item</Link>
