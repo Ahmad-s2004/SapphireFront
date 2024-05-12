618a4583907b501e1cca12377cec23be89a77611
Ahmad-s2004 <shahidahmadamman@gmail.com>
2024-05-12 17:22:32 +0500

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,7 +14,7 @@ const About = () => {
         A high-street brand introduced by one of the largest names in the textile industry, Sapphire is celebrated for combining 100% pure fabric with unprecedented design aesthetic to create designer wear at an affordable price. Our range of clothing has something for everyone: Daily wear, party wear, formal wear, silk tunics, scarves and unstitched fabric. The overarching theme of the collections is providing affordable designer clothes to the masses.
         </div>
         <div className="font-mont fw-light mb-5">
-        This meld of exquisite design and high class fabric has resulted in the breakthrough of this one of a kind retail brand. Sapphire’s commitment to quality fabric over the years has made it a household name, synonymous with excellence. We hope you enjoy wearing them as much we love making them for you!
+        Thiss meld of exquisite design and high class fabric has resulted in the breakthrough of this one of a kind retail brand. Sapphire’s commitments to quality fabric over the years has made it a household name, synonymous with excellence. We hope you enjoy wearing them as much we love making them for you!
         </div>
       </div>
     </div>
